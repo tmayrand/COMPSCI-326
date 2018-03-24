4d042604bf9a43cc26b121dd1fc9463bf00c5240
Shane Parr <sparr@umass.edu>
2018-03-24 12:41:46 -0400

    add urls

--- project2/Q/Cloq/urls.py
+++ project2/Q/Cloq/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('dashboard', views.dash, name ='dash'),
+    path('admin_dashboard', views.admin_dash, name = "admin_dash"),
+    path('availability', views.availability, name = "availability"),
+    path('schedule', views.schedule, name = "schedule"),
+    path('admin_schedule', views.admin_schedule, name = "admin_schedule"),
+    path('settings', views.settings, name = "settings"),
 ]
