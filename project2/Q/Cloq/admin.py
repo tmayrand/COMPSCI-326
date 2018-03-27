f8877d17ac8c7a4a3f071b065a153dbeebb9e223
T. Mayrand <tmayrand@users.noreply.github.com>
2018-03-27 08:10:10 -0400

    Fixed ID visability issue
    The unique ID for each object of a  model is now visible on the object page. @sparr1

--- project2/Q/Cloq/admin.py
+++ project2/Q/Cloq/admin.py
@@ -5,16 +5,19 @@ from .models import user, announcement, time
 @admin.register(user)
 class userAdmin(admin.ModelAdmin):
     list_display = ('uid', 'lastname', 'firstname')
-    fields = ['usertype', ('firstname', 'lastname'),
+    readonly_fields = ('uid',)
+    fields = [('uid', 'usertype'), ('firstname', 'lastname'),
               ('username', 'password'), 'email', 'pronoun',
               'phone', ('overtime', 'notification')]
 
 @admin.register(announcement)
 class announcementAdmin(admin.ModelAdmin):
     list_display = ('aid', 'title', 'time')
-    fields = ['title', 'text', 'time', 'usertype']
+    readonly_fields = ('aid',)
+    fields = [('title', 'aid'), 'text', 'time', 'usertype']
 
 @admin.register(time)
 class timeAdmin(admin.ModelAdmin):
     list_display = ('tid', 'start', 'end', 'timetype', 'uid')
-    fields = [('timetype', 'uid'), ('start', 'end')]
+    readonly_fields = ('tid',)
+    fields = [('timetype', 'uid', 'tid'), ('start', 'end')]
