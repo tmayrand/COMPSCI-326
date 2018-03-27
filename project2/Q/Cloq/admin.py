50f59b60f8000b9d930199dce3b5eae941a71466
T. Mayrand <tmayrand@users.noreply.github.com>
2018-03-26 23:27:15 -0400

    Er meh gerd, dater.
    Added 5 simlulated users and two admin accounts. Also added a couple of announcements sprinkled around random times.
    
    Added view orders in admin.py so we could actually see UID/AID/TID etc...

--- project2/Q/Cloq/admin.py
+++ project2/Q/Cloq/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import user, announcement, time
 
-admin.site.register(user)
-admin.site.register(announcement)
-admin.site.register(time)
+@admin.register(user)
+class userAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'lastname', 'firstname')
+    fields = ['usertype', ('firstname', 'lastname'),
+              ('username', 'password'), 'email', 'pronoun',
+              'phone', ('overtime', 'notification')]
+
+@admin.register(announcement)
+class announcementAdmin(admin.ModelAdmin):
+    list_display = ('aid', 'title', 'time')
+    fields = ['title', 'text', 'time', 'usertype']
+
+@admin.register(time)
+class timeAdmin(admin.ModelAdmin):
+    list_display = ('tid', 'start', 'end', 'timetype', 'uid')
+    fields = [('timetype', 'uid'), ('start', 'end')]
