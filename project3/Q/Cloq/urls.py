af9fd7878d505129d08045d5f14bbdf4adfb3af3
T. Mayrand <troy.mayrand@gmail.com>
2018-03-30 20:16:02 -0400

    Started poking@ proj3

---
+++ project3/Q/Cloq/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dash, name = 'home'),
+    path('dashboard', views.dash, name ='dash'),
+    path('admin_dashboard', views.admin_dash, name = "admin_dash"),
+    path('availability', views.availability, name = "availability"),
+    # path('schedule', views.schedule, name = "schedule"),
+    path('schedule/<int:year>/<int:month>/<int:day>/', views.schedule, name="schedule"),
+    path('admin_schedule', views.admin_schedule, name = "admin_schedule"),
+    path('settings', views.settings, name = "settings"),
+]
